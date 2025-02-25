dfe1d6a0e91f2658a78c5cf6e1bd4ec6015e7a99
lele-maxwell <leleivanlele22@gmail.com>
2025-02-25 18:04:12 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ use std::env;
 
 fn main() {
 
-    Println!("Hello world!");
+        println!("Hello, world!");
+
+
     
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
