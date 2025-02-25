803befabd6e1d0610d60e57d0b53d33031020a26
lele-maxwell <leleivanlele22@gmail.com>
2025-02-25 15:15:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{env::{self, args}, vec};
+
 fn main() {
     println!("Hello, world!");
+
+    let eneble_fib = env::args().nth(1).expect("input is required");
+    let max_threshold = env::args().nth(2).expect("input  is required");
+
+    println!( "\n enable_fib: {}  \n max_threshold: {} ", eneble_fib, max_threshold);
 }
